7df642358fa07c8cf35eb3faf8a3ae4ab476e6f7
Luis <lgmarchioreh@gmail.com>
2024-09-01 21:59:23 -0300

    style: Apply ESLint and Prettier formatting rules to all files

--- src/app/result/[makeId]/[year]/page.js
+++ src/app/result/[makeId]/[year]/page.js
@@ -1,7 +1,9 @@
-"use client"
 import { VehicleModels } from '@/components/Lists/CarList';
-import { Suspense } from 'react';
 import Link from 'next/link';
+import { Suspense } from 'react';
+import { generateStaticParams } from '@/lib/index';
+
+export { generateStaticParams };
 
 export default function ResultPage({ params }) {
   const { makeId, year } = params;
@@ -9,8 +11,13 @@ export default function ResultPage({ params }) {
   return (
     <div className="container mx-auto px-4 py-6">
       <div className="flex items-center justify-between mb-8">
-        <h1 className="text-4xl font-bold text-gray-800 mx-auto">Vehicle Models for {year}</h1>
-        <Link href="/" className="inline-block bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-lg">
+        <h1 className="text-4xl font-bold text-gray-800 mx-auto">
+          Vehicle Models for {year}
+        </h1>
+        <Link
+          href="/"
+          className="inline-block bg-gray-200 hover:bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded-lg"
+        >
           &larr; Back
         </Link>
       </div>
--- src/providers/react-query-provider.jsx
+++ src/providers/react-query-provider.jsx
@@ -1,13 +1,9 @@
-"use client";
-import React from "react";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+'use client';
+import React from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
 export function ReactQueryProvider({ children }) {
   const [client] = React.useState(new QueryClient());
 
-  return (
-    <QueryClientProvider client={client}>
-      {children}
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
